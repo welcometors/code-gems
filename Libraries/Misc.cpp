9c855a98bdad44b28fa4f65f40ba25628eb64119
welcometors <welcometors@gmail.com>
2016-04-15 11:55:47 +0530

    odd int digit sum

--- Libraries/Misc.cpp
+++ Libraries/Misc.cpp
@@ -27,3 +27,26 @@ unsigned long isqrt(unsigned long x){
 	}
 	return res;
 }
+
+unsigned long long digsumodd(int n) {
+	typedef unsigned long long u64;
+	static u64 predigsum[10] = { 0, 25, 475, 7000, 92500, 1150000, 13750000, 160000000, 1825000000, 20500000000 };
+	static u64 predigcnt[10] = { 0, 5, 50, 500, 5000, 50000, 500000, 5000000, 50000000, 500000000 };
+	static u64 presumnum[10] = { 0, 1, 3, 6, 10, 15, 21, 28, 36, 45 };
+	static u64 presumodd[10] = { 0, 1, 1, 4, 4, 9, 9, 16, 16, 25 };
+
+	int scale = 0;
+	for (int x = n; x; x /= 10)scale++;
+
+	u64 sum = 0;
+	while (--scale) {
+		int d = (n / predigcnt[scale]) / 2;
+		if (d)
+			sum += predigsum[scale] * d + presumnum[d - 1] * predigcnt[scale];
+		n -= predigcnt[scale] * 2 * d;
+		if (d)
+			sum += ((n + 1) / 2)*d;
+	}
+	sum += presumodd[n];
+	return sum;
+}
